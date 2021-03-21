125d570ad9b62bdf5c7a9e82ff15d8dc61e0a8fe
Bisma Satria <bismasatrian@gmail.com>
2021-03-21 10:54:35 +0700

    Init Repo

---
+++ app/src/main/java/com/example/navigation_android/ProfielActivity.kt
@@ -0,0 +1,11 @@
+package com.example.navigation_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfielActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profiel)
+    }
+}
